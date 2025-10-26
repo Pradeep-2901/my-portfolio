6d0688c82c13fbe3e4c5cfbc8cf26b657472b898
Pradeep-2901 <86100909+Pradeep-2901@users.noreply.github.com>
2025-10-26 09:28:01 +0530

    feat: Initial project setup with Vite, React, TS, and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-gray-900">
+      <h1 className="text-3xl font-bold text-white">
+        Hello, World! My portfolio starts today.
+      </h1>
+    </div>
+  )
+}
+
+export default App
